9ca71d47b5b848e35821b8a0d3c7f8c8bcfafa13
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-09-30 00:36:45 -0700

    Add map of Irina

--- pm/locales/irina.poly.pm
+++ pm/locales/irina.poly.pm
@@ -7,5 +7,7 @@
 
     While no longer the naval superpower it once was, Irina still controls the primary shipping route to Elora, the Undercrossing. This route winds upwards through the vast caverns beneath Avrasol, climbing upwards until ships reach the caverns' mouth in Ethle l'Aciale @ipa['w]{ˈeɬ̪e laˈtsjale}, the Iron Valley. The caverns are constantly shifting, old routes collapsing and new routes opening. In order to make the crossing, a ship must hire an Irinan pilot from the Shipmasters' Guild, lest they dare tempt fate in the caves.
 
-    @paragraph["Azathano"]{Azathano trees are large, trees with broad leaves native to Irina.}
+    @figure[#:class "fullwidth"]{
+        @img[#:src "/assets/Irina.min.svg"]
+    }
 }
--- rkt/constants.rkt
+++ rkt/constants.rkt
@@ -1,6 +1,9 @@
 #lang racket
 (provide (all-defined-out))
 
+;;; The sigil.
+(define at-sign "@")
+
 ;;; The symbol used when margin notes are hidden on small screens.
 (define margin-note-symbol "◊")
 (define margin-figure-symbol "☙ ❧")
